62049f5d6ed68c35ecb0704fa6935776c5094e94
Olly Freeman <ollyfreeman209@gmail.com>
2015-05-30 13:44:18 -0700

    grunt-watch runs nodeunit correctly

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
             files: ['**/*.coffee', 'etc/**'],
             tasks: ['default'],
             options: {
-              spawn: false,
+              spawn: true,
             },
           },
         }
